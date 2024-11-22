504cdd11911ef635c463d7de91631105e6729c1d
Muhammad Salman <155625508+Salman-Afzal00@users.noreply.github.com>
2024-11-22 12:34:24 +0500

    commit by 1180

--- orders.cpp
+++ orders.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
 int main(){
     int i = 1;
     cout <<"helooooooo";
